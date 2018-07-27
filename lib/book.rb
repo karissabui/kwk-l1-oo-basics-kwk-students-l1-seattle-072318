3df846970eec003289c1f4a7a8721fc31d91007a
Karissa Bui <karissab002@gmail.com>
2018-07-27 16:21:07 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,6 +3,7 @@
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
+  
   def initialize(title)
     @title = title
   end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,11 @@
 # Make your shoe class here!
+
+class Shoe 
+  attr_accessor :size, :color, :type, :material, :condition
+  attr_reader :brand
+  
+  def initialize(brand)
+    @brand = brand
+  end
+
+end
